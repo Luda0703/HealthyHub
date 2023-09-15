bed829cb783a7e1790957270a4f8c73ecbb1d375
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-09-15 09:17:28 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import React from 'react';
+import { YourGoal } from './YourGoal/YourGoal'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <YourGoal/>
     </div>
   );
 };
