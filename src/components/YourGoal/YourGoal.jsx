91c95ec61339ce3105cdb82e822e6c272f5ba4be
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-09-16 12:54:39 +0300

    update

--- src/components/YourGoal/YourGoal.jsx
+++ src/components/YourGoal/YourGoal.jsx
@@ -1,70 +1,74 @@
 import React from 'react';
-import YourGoalsDiskt from '../../images/YourGoals-diskt.png';
-// import YourGoalsDiskt2x from '../../images/YourGoals-diskt@2x.png';
-import styles from './YourGoalStyle.css'
+import image1 from '@/pages/SignUpPage/images/YourGoals-diskt.png';
+import image2 from '@/pages/SignUpPage/images/YourGoals-diskt@2x.png';
+import image3 from '@/pages/SignUpPage/images/YourGoals-tab.png';
+import image4 from '@/pages/SignUpPage/images/YourGoals-tab@2x.png';
+import image5 from '@/pages/SignUpPage/images/YourGoals-mob.png';
+import image6 from '@/pages/SignUpPage/images/YourGoals-mob@2x.png';
 
-// const small = 'YourGoals-mob.png';
-// const medium = 'YourGoals-tab.png';
-// const large = 'YourGoals-diskt.png';
+import styles from "./yourGoal.module.scss";
+import { Formik, Form, Field } from 'formik';
 
-export const YourGoal = () => {
+const YourGoal = () => {
+  const handleSubmite = (values, actions) => {
+
+  }
   return (
-    <div>
-      <div>
-        {/* <picture>
+
+    <div className={styles.container_your_goal}>
+      <div className={styles.container_picture}>
+        <picture className={styles.picture}>
           <source
-            media="(min-width: 700px)"
-            srcSet="YourGoals-diskt.png 1x, YourGoals-diskt@2x.png 2x"
+            media="(min-width: 1440px)"
+            srcSet={`${image1} 1x, ${image2} 2x`}
+            width={592} height={574}
           />
           <source
-            media="(min-width: 450px)"
-            srcSet="YourGoals-tab.png 1x, YourGoals-tab@2x.png 2x"
+            media="(min-width: 833px)"
+            srcSet={`${image3} 1x, ${image4} 2x`}
+            width={380} height={368}
           />
           <img
-          loading="lazy"
-            srcSet="YourGoals-mob.png 1x, YourGoals-mob@2x.png 2x"
-            src="YourGoals-mob.png"
+          // loading="lazy"
+          srcSet={`${image5} 1x, ${image6} 2x`}
+          width={300} height={290}
+            src={image5}
             alt="Your Goals"
           />
-        </picture> */}
-        <img 
-        // srcSet="
-        // YourGoals-diskt.png 444w,
-        // YourGoals-diskt@2x.png 444w,
-        // YourGoals-tab.png 380w,
-        // YourGoals-tab@2x.png 380w,
-        // YourGoals-mob.png 300w,
-        // YourGoals-mob@2x.png 300w
-        // "
-        // src={small}
-        // srcSet={`${small} 300w, ${medium} 380w, ${large} 444w`}
-        sizes="(max-width: 320px) 300px,
-            (max-width: 480px) 380px,
-            444px"
-        src={YourGoalsDiskt} 
-        // src='YourGoals-diskt.png'
-        alt='Your Goals'
-        />
+        </picture>
       </div>
-      <div>
-        <h1>Your goal</h1>
-        <form>
-          <p>Choose a goal so that we can help you effectively</p>
-          <label>
-            <input type="radio" name="check" value="Lose Fat" />
+      <div className={styles.container_form}>
+        <h1 className={styles.title}>Your goal</h1>
+        <Formik onSubmit={handleSubmite}>
+        <Form className={styles.form_checked}>
+          <p className={styles.text}>Choose a goal so that we can help you effectively</p>
+          <div className={styles.container_label_list}>
+          <div className={styles.container_label}>
+          <label className={styles.list}>
+            <Field className={styles.item} type="radio" name="check" value="Lose Fat " onChange = {()=>{}}/>
             Lose Fat
           </label>
-          <label>
-            <input type="radio" name="check" value="Maintain" checked />
+          </div>
+          <div className={styles.container_label_ctnter}>
+          <label className={styles.list}>
+            <Field className={styles.item} type="radio" name="check" value="Maintain " onChange = {()=>{}}  />
             Maintain
           </label>
-          <label>
-            <input type="radio" name="check" value="Gail Muscle" />
+          </div>
+          <div className={styles.container_label_end}>
+          <label className={styles.list}>
+            <Field className={styles.item} type="radio" name="check" value="Gail " onChange = {()=>{}}/>
             Gail Muscle
           </label>
-        </form>
-        <button>Next</button>
+          </div>
+          </div>
+          <button type="submit" className={styles.btn}>Next</button>
+        </Form>
+        </Formik>
       </div>
     </div>
   );
 };
+
+export default YourGoal;
+
